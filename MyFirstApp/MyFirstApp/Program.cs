508d34a0f144befd1bb94d220efc39401332a8fa
Saurabh Kumar <saurabhk2323@gmail.com>
2023-12-01 00:09:46 +0530

    learning completed for HTTP with reading values from body, passing query strings as body

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Primitives;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,42 +7,21 @@ var app = builder.Build();
 // context contains the information about request/response with other information
 app.Run(async (HttpContext context) =>
 {
-    context.Response.Headers["Content-Type"] = "text/html";
+    StreamReader reader = new StreamReader(context.Request.Body);
+    string body = await reader.ReadToEndAsync();
 
-    // reads path of the url
-    // we can build our logic based on path
-    string path = context.Request.Path;
+    // read query string values sent in body
+    // converts query strings into dictinary object
+    // reason behind StringValues: Query strings might consist more than one same key with different values
+    Dictionary<string, StringValues> queryDict = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
 
-    // reads mathod of the request
-    string method = context.Request.Method;
-
-    // ? separates path and query string || & separates two query strings
-    // /dashboard?id=1&name=rahul : path = /dashboard, query string: id=1 || name = rahul
-
-    if(method == "GET")
+    if(queryDict.ContainsKey("age"))
     {
-        // context.Request.Query: A dictionary containing key value pair of query string & their values
-        if(context.Request.Query.ContainsKey("id"))
-        {
-            string id = context.Request.Query["id"];
-            await context.Response.WriteAsync($"<h1>{id}</h1>");
-        }
+        string age = queryDict["age"][0];
+        await context.Response.WriteAsync(age);
     }
 
-    // Accept: represents MIME type of response content to be accepted by the client | it generally tells server, i can take response in this type only
-    // Accept-Language: language of response content
-    // Host: server domain name
-    // User-Agent: the type of browser from which request is coming to server
-    // Cookie: contains cookie to send browser
-
-    if(context.Request.Headers.ContainsKey("User-Agent"))
-    {
-        string userAgent = context.Request.Headers["User-Agent"];
-        await context.Response.WriteAsync($"<h2>{userAgent}</h2>");
-    }
-
-    await context.Response.WriteAsync($"<p>{path}</p>");
-    await context.Response.WriteAsync($"<p>{method}</p>");
+    await context.Response.WriteAsync(body);
 });
 
 app.Run();
