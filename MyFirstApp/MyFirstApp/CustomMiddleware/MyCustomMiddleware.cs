43123909ba71f96815e7a0a0cb1120d1f8abeb24
Saurabh Kumar <saurabhk2323@gmail.com>
2023-12-01 08:07:35 +0530

    learnt the implementation and execution of middleware, how to use custom middleware

---
+++ MyFirstApp/MyFirstApp/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace MyFirstApp.CustomMiddleware
+{
+    // implement IMiddleware to use this class as Middleware
+    // this needs to be registered as well in Program.cs | under services
+    public class MyCustomMiddleware : IMiddleware
+    {
+        // same arguments as app.Use()
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("<div>My Custom Middleware - Starts</div>");
+            await next(context);
+            await context.Response.WriteAsync("<div>My Custom Middleware - Ends</div>");
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app) => app.UseMiddleware<MyCustomMiddleware>();
+    }
+}
--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,27 +1,48 @@
-using Microsoft.Extensions.Primitives;
+using MyFirstApp.CustomMiddleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// register middleware in services
+builder.Services.AddTransient<MyCustomMiddleware>();
+
 var app = builder.Build();
 
-// Gets created automatically on receiving Http request
-// context contains the information about request/response with other information
+
+// it does not forward request to subsequent middleware
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Hello ");
+//});
+
+// Middleware - 1
+// so how to achieve chaining of middleware --> take help from "Use"
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    context.Response.Headers["content-type"] = "text/html";
+    await context.Response.WriteAsync("<div>Middleware 1 starts </div>");
+    await next(context); // passes context to next middleware
+    await context.Response.WriteAsync("<div>Middleware 1 ends</div>");
+});
+
+// Middleware - 2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("<div>Middleware 2 starts</div>");
+    await next(context);
+    await context.Response.WriteAsync("<div>Middleware 2 ends</div>");
+});
+
+// Middleware - 3
+// custom middleware
+// this implementation can be simplified further using extension methods
+//app.UseMiddleware<MyCustomMiddleware>();
+app.UseMyCustomMiddleware();
+
+// Middleware - 4
+// terminating middleware
 app.Run(async (HttpContext context) =>
 {
-    StreamReader reader = new StreamReader(context.Request.Body);
-    string body = await reader.ReadToEndAsync();
-
-    // read query string values sent in body
-    // converts query strings into dictinary object
-    // reason behind StringValues: Query strings might consist more than one same key with different values
-    Dictionary<string, StringValues> queryDict = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
-
-    if(queryDict.ContainsKey("age"))
-    {
-        string age = queryDict["age"][0];
-        await context.Response.WriteAsync(age);
-    }
-
-    await context.Response.WriteAsync(body);
+    await context.Response.WriteAsync("<div>Terminating Middleware </div>");
 });
 
 app.Run();
