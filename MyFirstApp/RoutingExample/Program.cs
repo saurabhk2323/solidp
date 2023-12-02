bca765eccb9d06890a0635f11c99c30a7130737f
Saurabh Kumar <saurabhk2323@gmail.com>
2023-12-02 23:17:15 +0530

    route constraint completed

--- MyFirstApp/RoutingExample/Program.cs
+++ MyFirstApp/RoutingExample/Program.cs
@@ -7,6 +7,7 @@ app.UseRouting();
 // Route Paramters - {employeeName}
 // default paramters - {id=1}
 // optional parameters - {id?}
+// constraints - put some restriction over parameters \ check endpoint only if constraint is matched \
 app.UseEndpoints(endpoints =>
 {
     // define route paramters as shown below --> filename and extension can be anything
@@ -42,6 +43,64 @@ app.UseEndpoints(endpoints =>
         int cid = Convert.ToInt32(context.Request.RouteValues["cid"]);
 
         await context.Response.WriteAsync($"Customer details - {cid}");
+    });
+
+    // constraint over paramters
+    // {id:int}, {active:bool}, {date:datetime} matches with 2030-01-01%2011:59%20pm
+    // decimal
+    // long
+    // guid - a hexadecimal number that is universally unique
+    // minlenght(value)
+    //maxlength(value)
+    //length(min,max)
+    endpoints.Map("blogs/authors/{id:int?}", async (context) =>
+    {
+        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+
+        await context.Response.WriteAsync($"blogs author id - {id}");
+    });
+
+    endpoints.Map("daily-digest-report/{reportDate:datetime}", async (context) =>
+    {
+        DateTime? datetime = Convert.ToDateTime(context.Request.RouteValues["reportDate"]);
+
+        await context.Response.WriteAsync($"daily-digest-report time: {datetime}");
+    });
+
+    // cities/guid --? not cities/city/guid
+    endpoints.Map("cities/{cityid:guid}", async context =>
+    {
+        string? guid = Convert.ToString(context.Request.RouteValues["cityid"]);
+
+        await context.Response.WriteAsync($"cityid - {guid}");
+    });
+
+    // minlenght(value)
+    // maxlength(value)
+    // length(min,max)
+    // length(value)
+    // min(value) - matches with an integer value
+    // max(value)
+    // range(min, max)
+    // alpha - only alphabets
+    // regex(expression)
+    // endpoints.Map("users/{username:minlength(5)=harshal}", async context =>
+    // endpoints.Map("users/{username:maxlength(5)=harshal}", async context =>
+    endpoints.Map("users/{username:length(5,10)=harshal}", async context =>
+    {
+        string? username = Convert.ToString(context.Request.RouteValues["username"]);
+
+        await context.Response.WriteAsync($"username: {username}");
+    });
+
+    // regex
+    endpoints.Map("sales-report/{year:int:min(1900)}/{month:regex(^(apr|jul|oct|jan)$)}", async context =>
+    {
+        int year = Convert.ToInt32(context.Request.RouteValues["year"]);
+        string? month = Convert.ToString(context.Request.RouteValues["month"]);
+
+        await context.Response.WriteAsync($"sales report - {year}/{month}");
+
     });
 });
 
