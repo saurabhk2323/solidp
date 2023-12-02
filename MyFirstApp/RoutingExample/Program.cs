06bd55fcc20111ffce60013eaab481279f9e7041
Saurabh Kumar <saurabhk2323@gmail.com>
2023-12-02 21:22:16 +0530

    learnt GetEndpoints

--- MyFirstApp/RoutingExample/Program.cs
+++ MyFirstApp/RoutingExample/Program.cs
@@ -1,12 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// GetEndPoint -> displayName & requestDelegate
+// when called before "useRouting" -> returns null
+app.Use(async (context, next) =>
+{
+    Endpoint endpoint = context.GetEndpoint();
+    await next(context);
+});
+
 // enable routings
 // along with this, we must have to use map* as well
 app.UseRouting();
 
+// GetEndPoint -> displayName & requestDelegate
+// when called before "useRouting" -> returns null
+// Note: if none of the defined endpoints matched, GetEndpoint returns null only
+app.Use(async (context, next) =>
+{
+    Endpoint endpoint = context.GetEndpoint(); // map1 HTTP: GET
+    await next(context);
+});
+
 // creating endpoints
 // first of all endpoints are matched here
+// if anyone endpoint matched, it doesn't execute others
 app.UseEndpoints(endpoints =>
 {
     // add your endpoints here
